635b40cbe4e6da30b48fdf74aecc6aac4adc30b5
Hurumnanya <105320491+Hurumnanya@users.noreply.github.com>
2022-06-23 11:35:25 +0100

    javascript calculator

---
+++ jscalculator.js
@@ -0,0 +1,17 @@
+var opr = prompt('operator: ')
+var num1 = prompt("firstnum: ")
+var num2 = prompt("secondnum: ")
+
+if (opr == "+")
+{
+    console.log(num1 + num2)
+} else if (opr == "-")
+{
+    console.log(num1 - num2)
+} else if (opr == "*")
+{
+    console.log(num1 * num2)
+} else if (opr == "/")
+{
+    console.log(num1 / num2)
+}
